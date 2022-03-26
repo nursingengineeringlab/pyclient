b8d17a145e44983e18ecd4190e0232f4a34d289a
Shiyang Wang <shiyangwang@umass.edu>
2022-03-25 23:39:11 -0400

    update config

--- config.py
+++ config.py
@@ -1,24 +1,13 @@
-
-<<<<<<< HEAD
-auth_token = '9ae957eef83acf25817b315444be946a6d6826c4'
-=======
 auth_token = '9208805fe4ba3e2d89f33777fc969d2727f26351'
->>>>>>> c400e03a0015753ddd482d24a519f7f4922ac340
 request_headers = {'Content-Type': 'application/json', 'Authorization': 'Token '+ auth_token}
 test_device_id      = "F43053011ACF"
 test_device_type    = "RRI"
 filename = "./data_store/test.txt"
 
 
-<<<<<<< HEAD
-# base_ip = "nelab.ddns.umass.edu"
-base_ip = "20.231.78.167"
-base_port = "8000"
-=======
 base_ip = "nelab.ddns.umass.edu"
 mqtt_port = "30482"
 base_port = "31000"
->>>>>>> c400e03a0015753ddd482d24a519f7f4922ac340
 base_url = "http://" + base_ip + ":" + base_port + "/"
 ws_url = "ws://" + base_ip + ":" + base_port + "/ws/sensor/RR"
 
